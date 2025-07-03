c2f889177c836e5ebbf3fe2553591b6abd0e89d5
ShevaMellany <128024052+shevaamellany@users.noreply.github.com>
2025-07-03 14:28:11 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 // lib/main.dart
 
 import 'package:flutter/material.dart';
-import 'package:budget_mate_app/screens/dashboard_screen.dart'; // Import halaman dashboard baru kita
+import 'package:budget_mate_app/screens/dashboard_screen.dart'; // Import halaman dashboard
 
 void main() {
-  runApp(const MyApp()); // Jalankan aplikasi Flutter
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,33 +13,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BudgetMate', // Judul aplikasi
+      title: 'BudgetMate',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green, // Menggunakan warna hijau sebagai warna utama tema
-        primaryColor: Colors.green.shade700, // Warna primer yang lebih spesifik
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.green.shade700, // Warna latar belakang AppBar
-          foregroundColor: Colors.white, // Warna teks dan ikon di AppBar
-          titleTextStyle: const TextStyle(
-            color: Colors.white,
+        useMaterial3: true,
+        primaryColor: const Color(0xFFC4E860), // Warna lemon
+        scaffoldBackgroundColor: const Color(0xFFFDFDFD),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFFC4E860), // Warna AppBar
+          foregroundColor: Colors.black, // Warna teks/icon
+          titleTextStyle: TextStyle(
+            color: Colors.black,
             fontSize: 22,
             fontWeight: FontWeight.bold,
           ),
         ),
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Colors.green.shade600, // Warna latar belakang Floating Action Button
-          foregroundColor: Colors.white, // Warna ikon/teks di FAB
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Color(0xFFC4E860),
+          foregroundColor: Colors.black,
         ),
-        cardTheme: CardThemeData( // Menggunakan CardThemeData untuk tema kartu
+        cardTheme: CardTheme(
           elevation: 4,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12.0), // Sudut membulat untuk semua Card
+            borderRadius: BorderRadius.circular(12.0),
           ),
         ),
-        useMaterial3: true, // Mengaktifkan Material Design 3
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xFFC4E860),
+          secondary: Colors.deepOrangeAccent,
+        ),
       ),
-      home: const DashboardScreen(), // Halaman awal yang akan ditampilkan adalah DashboardScreen
-      debugShowCheckedModeBanner: false, // Sembunyikan banner "DEBUG"
+      home: const DashboardScreen(),
     );
   }
 }
